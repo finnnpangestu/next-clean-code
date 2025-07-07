6a8f966c74e5a64d14a1e92bcfc04ac359ce8b01
finnnpangestu <muhammadalfinpangestu@gmail.com>
2025-07-07 18:15:01 +0700

    gabut

--- src/modules/user/components/user_card.tsx
+++ src/modules/user/components/user_card.tsx
@@ -7,6 +7,7 @@ export const UserCard = ({ user }: Props) => {
     <div className="p-4 border rounded shadow">
       <h3 className="text-lg font-bold">{user.name}</h3>
       <p className="text-sm text-gray-500">{user.email}</p>
+      <p className="text-sm text-gray-500">{user.address?.street} | {user.address?.city}</p>
     </div>
   );
-}
+};
